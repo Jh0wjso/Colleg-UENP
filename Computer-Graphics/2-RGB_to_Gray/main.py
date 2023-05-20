6ea82cfe47cd6dfc1910e6579613e7bbc265013c
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-05-19 22:23:10 -0300

    Second code Problem solved

---
+++ Computer-Graphics/2-RGB_to_Gray/main.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+def converter_para_tons_de_cinza(caminho_imagem):
+    imagem = Image.open(caminho_imagem)
+
+    imagem_cinza = imagem.convert('L')
+
+    caminho_imagem_cinza = caminho_imagem.replace('.png', '_cinza.png')
+
+    imagem_cinza.save(caminho_imagem_cinza)
+
+    return caminho_imagem_cinza
+
+caminho_imagem_colorida = '/home/jhonatan/Documentos/UENP/CG/2/carro.jpg'
+caminho_imagem_cinza = converter_para_tons_de_cinza(caminho_imagem_colorida)
+print("Caminho da imagem em tons de cinza:", caminho_imagem_cinza)
